1b56875ee3a0fda8994f2fb0bafa70023d1a6bb6
Academy_learner <academy_learner@Academy-learners-MacBook-Air.local>
2021-12-02 16:13:11 +0200

    my fisrt code

---
+++ test.js
@@ -0,0 +1,33 @@
+console.log("I AM LINDOKUHKLE")
+var num = 5;
+var otherNum = 10;
+var sum;
+sum = num + otherNum;
+
+//prompt("what is your name?");
+console.log(sum)
+if(sum==15)
+{
+    console.log("Correct")
+}
+else
+{
+    console.log("incorrect")
+}
+for(var x=0;x<sum;x++)
+{
+    if(x < 2)
+    {
+        console.log(x)
+    }
+    else if(x > 12)
+    {
+        console.log(x)
+    }
+    
+}
+while(sum==15)
+{
+    console.log("you did great LINDO")
+    sum += 2;
+}
